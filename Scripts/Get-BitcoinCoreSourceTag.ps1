386ed3c33c0c3c76d21d254413e17f1422525d0a
Matt Gucci <matt9ucci@gmail.com>
2019-10-12 09:26:51 +0900

    Use splatting in Get-BitcoinCoreSourceTag.ps1

--- Scripts/Get-BitcoinCoreSourceTag.ps1
+++ Scripts/Get-BitcoinCoreSourceTag.ps1
@@ -2,7 +2,13 @@ param (
 	[switch]$Name
 )
 
-$result = Invoke-RestMethod -Uri https://api.github.com/repos/bitcoin/bitcoin/tags -Method Get -FollowRelLink
+$param = @{
+	Uri = 'https://api.github.com/repos/bitcoin/bitcoin/tags'
+	Method = 'Get'
+	FollowRelLink = $true
+}
+$result = Invoke-RestMethod @param
+
 if ($Name) {
 	$result.name
 } else {
